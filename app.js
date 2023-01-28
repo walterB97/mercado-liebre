1229e0a2fbb0a1b6814b1f3dea633942329de6c3
walterB97 <121910127+walterB97@users.noreply.github.com>
2023-01-27 22:26:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express= require("express")
+const app= express();
+const path = require("path")
+
+const publicPath=path.join(__dirname, "public");
+app.use(express.static('public'));
+
+app.get("/",(req, res) =>
+res.sendFile(path.join(__dirname, "/views/home.html")))
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
