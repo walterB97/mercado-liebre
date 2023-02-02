6756e8620b221ed67a2dcf259467c1d57502c201
walterB97 <121910127+walterB97@users.noreply.github.com>
2023-02-02 19:27:11 -0300

    Add files via upload
    ultimos cambios de mercado liebre

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.static('public'));
 app.get("/",(req, res) =>
 res.sendFile(path.join(__dirname, "/views/home.html")))
 
+app.get("/register",(req, res) =>
+res.sendFile(path.join(__dirname, "/views/register.html")))
+
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
