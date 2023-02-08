830bfccfae15a0ee44fbff339d435ba1d017c911
walterB97 <wbarboza97@gmail.com>
2023-02-08 12:09:47 -0300

    correcciones

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path")
 const publicPath=path.join(__dirname, "public");
 app.use(express.static('public'));
 
-app.get("/",(req, res) =>
+app.get("/home",(req, res) =>
 res.sendFile(path.join(__dirname, "/views/home.html")))
 
 app.get("/register",(req, res) =>
