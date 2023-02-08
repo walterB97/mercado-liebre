d0d582f3648d86c46f5686ef45a17795fc24aad4
walterB97 <121910127+walterB97@users.noreply.github.com>
2023-02-08 00:19:46 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ res.sendFile(path.join(__dirname, "/views/home.html")))
 app.get("/register",(req, res) =>
 res.sendFile(path.join(__dirname, "/views/register.html")))
 
+app.get("/login",(req, res) =>
+res.sendFile(path.join(__dirname, "/views/login.html")))
 
-const port = 3000;
+
+const port = process.env.PORT||3000;
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
